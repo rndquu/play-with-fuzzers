410a63b0c1156275f1b7c3f577e0afbcd06301b9
rndquu <rndquu@gmail.com>
2024-05-08 17:43:32 +0300

    test: add Deposit invariant example

---
+++ test/Deposit.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import "forge-std/Test.sol";
+import "../src/Deposit.sol";
+
+contract DepositInvariantTest is Test {
+    Deposit deposit;
+
+    function setUp() external {
+        deposit = new Deposit();
+        vm.deal(address(deposit), 100 ether);
+    }
+
+     function invariant_alwaysWithdrawable() external payable {
+        vm.startPrank(address(0xaa));
+        vm.deal(address(0xaa), 10 ether);
+        deposit.deposit{value: 1 ether}();
+        uint256 balanceBefore = deposit.balance(address(0xaa));
+        vm.stopPrank();
+        
+        assertEq(balanceBefore, 1 ether);
+
+        vm.prank(address(0xaa));
+        deposit.withdraw();
+        uint256 balanceAfter = deposit.balance(address(0xaa));
+
+        vm.stopPrank();
+        assertGt(balanceBefore, balanceAfter);
+    }
+
+    receive() external payable {}
+}
